762e41f9c3191452ebd5c59ff7a2ca8b2f7b94e6
singhravi5590 <singhravi5590@gmail.com>
2025-07-22 17:15:12 +0530

    Navbar Completed

---
+++ Frontend/src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+        <img src="#" alt="GTA" />
+        <div className='link-container'>
+            <a href="">Home</a>
+            <a href="">About Us</a>
+            <a href="">Taxi Rental</a>
+            <a href="">Airport Transfer</a>
+        </div>
+        <div className='contact'>
+            <button href="">Contact Us</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
