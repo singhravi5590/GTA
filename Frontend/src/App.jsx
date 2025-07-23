7e288012298bd511cdb75f3fdf76ee6166971452
singhravi5590 <singhravi5590@gmail.com>
2025-07-23 23:02:07 +0530

    Some changes on navbar and Home Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Home from './Components/Home'
 import './App.css'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Home/>
     </div>
   )
 }
