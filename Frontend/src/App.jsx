30951ac2dc24c8c5713b89a17e5526285d5e316c
singhravi5590 <singhravi5590@gmail.com>
2025-08-20 22:19:17 +0530

    Footer Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 import './App.css'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Home/>
+      <Footer/>
     </div>
   )
 }
--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -83,6 +83,7 @@ const Home = () => {
 
       <div className='line'></div>
       <div className='line'></div>
+      
 
 
 
