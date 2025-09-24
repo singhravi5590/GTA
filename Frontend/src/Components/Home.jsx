714a2cb4b6b5c1ca044c6b9dab1093cc7fa27570
singhravi5590 <singhravi5590@gmail.com>
2025-09-24 23:47:21 +0530

    pushed

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -105,6 +105,8 @@ const Home = () => {
       <div className='line'></div>
       <div className='line'></div>
       <div className='line'></div>
+      <div className='line'></div>
+      <div className='line'></div>
       
 
 
